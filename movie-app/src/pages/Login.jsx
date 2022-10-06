20a9e1f06f55e4a311f034e88a3ced01d18f8221
salih <muhammetsalihaslan@gmail.com>
2022-10-06 11:52:09 +0300

    login

--- movie-app/src/pages/Login.jsx
+++ movie-app/src/pages/Login.jsx
@@ -1,9 +1,69 @@
-
+import React, {useState} from 'react';
 
 const Login = () => {
+    
+    const [email, setEmail] = useState();
+    const [password, setPassword] = useState();
+
+    const handleSubmit = (e) => {
+      e.preventDefault();
+      console.log(email, password);
+    }
     return(
-        <div>
-            
+        <div className="d-flex justify-content-center">
+            <div className="form-image d-none d-md-block">
+                <img src={'https://picsum.photos/800/800'} alt="sample-movie" />
+
+            </div>
+            <div className="register-form">
+                 <h1 className="form-title display-3">Login</h1>
+                 <form id="Login" onSubmit={handleSubmit}>
+                    
+
+                   
+                    
+                    <div className="mb-3">
+
+                    <label htmlFor="email" className="form-label">E-mail</label>
+                    <input 
+                    type="email"
+                    className="form-control"
+                    id="email"
+                    placeholder="Enter your e-mail"
+                    required
+                    onChange={(e) => setEmail(e.target.value)}
+                    />
+
+                    </div>
+                
+                    <div className="mb-3">
+
+                    <label htmlFor="password" className="form-label">Password</label>
+                    <input 
+                    type="password"
+                    className="form-control"
+                    id="password"
+                    placeholder="Enter your password"
+                    required
+                    onChange={(e) => setPassword(e.target.targetvalue)}
+                    />
+
+                    </div>
+
+                    <input 
+                    type="submit" 
+                    className="btn btn-primary form-control"
+                    value="Login"/>
+
+                    <input 
+                    type="submit" 
+                    className="btn btn-primary form-control"
+                    value="Continue with Google"/>
+                    
+                
+                </form>
+            </div>
+
         </div>
     )
 }
