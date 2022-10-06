ed617c2b335dd4f5bfbddc5e5fad2efbc821bc39
salih <muhammetsalihaslan@gmail.com>
2022-10-06 11:25:29 +0300

    register 2

--- movie-app/src/pages/Register.jsx
+++ movie-app/src/pages/Register.jsx
@@ -11,33 +11,57 @@ const Register = () => {
             <div className="register-form">
                  <h1 className="form-title display-3">Register</h1>
                  <form id="register">
+                    <div className="mb-3">
+
                     <label htmlFor="firstName" className="form-label">First Name</label>
                     <input 
                     type="text"
                     className="form-control"
                     id="firstName"
-                    placeholder="Enter your first name"/>
-                
+                    placeholder="Enter your first name"
+                    required/>
+                    </div>
+
+                    <div className="mb-3">
+
                     <label htmlFor="lastName" className="form-label">Last Name</label>
                     <input 
                     type="text"
                     className="form-control"
                     id="lastName"
-                    placeholder="Enter your last name"/>
+                    placeholder="Enter your last name"
+                    required/>
+                    </div>
                     
+                    <div className="mb-3">
+
                     <label htmlFor="email" className="form-label">E-mail</label>
                     <input 
-                    type="text"
+                    type="email"
                     className="form-control"
                     id="email"
-                    placeholder="Enter your e-mail"/>
-                    
+                    placeholder="Enter your e-mail"
+                    required/>
+
+                    </div>
+                
+                    <div className="mb-3">
+
                     <label htmlFor="password" className="form-label">Password</label>
                     <input 
-                    type="text"
+                    type="password"
                     className="form-control"
                     id="password"
-                    placeholder="Enter your password"/>
+                    placeholder="Enter your password"
+                    required/>
+
+                    </div>
+
+                    <input 
+                    type="submit" 
+                    className="btn btn-primary form-control"
+                    value="Register"/>
+                    
                 
                 </form>
             </div>
