866aba58cc0707c3c8554349c0723088cc81a26d
salih <muhammetsalihaslan@gmail.com>
2022-10-12 14:45:51 +0300

    main

--- movie-app/src/pages/Main.jsx
+++ movie-app/src/pages/Main.jsx
@@ -1,10 +1,34 @@
+import React, { useEffect } from 'react';
+import { useState } from 'react';
+import axios from 'axios';
 
-
+const API_KEY = `0db9423930517eebdf7f4f1ffbd6b266`
+const FEATURED_API = `https://api.themoviedb.org/3/discover/movie?api_key=${API_KEY}`;
+const SEARCH_API = `https://api.themoviedb.org/3/search/movie?api_key=${API_KEY}&query=`;
 
 const Main = () => {
-    return(
-        <div>
+    const [movies, setMovies] = useState([]);
+    const [loading, setLoading] = useState(false);
 
+    useEffect(() => {
+        getMovies(FEATURED_API);
+    }, []);
+
+
+    const getMovies = (API) => {
+        setLoading(true)
+        axios.get(API).then((res) => console.log(res.data.results)).catch((err) => console.log(err)).finally(() => setLoading(false))
+    };
+    
+    return(
+        <div className="d-flex justify-content-center flex-wrap">
+         {loading ? (
+          <div className="spinner-border text-primary m-5" role="status">
+            <span className="sr-only">Loading...</span>
+          </div>
+        ) : (
+          movies?.map((movie) => null)
+        )}
         </div>
     ) 
 }
